b3566baf55bc6923dd4494da3a88f9888ef9c284
marvikk <russzkij@yahoo.com>
2017-01-27 23:21:01 +0200

    dot

--- app/app.js
+++ app/app.js
@@ -30,8 +30,8 @@ angular.module('myApp', [
         $rootScope.Id = $cookieStore.get('Id');
         $rootScope.isAuthorization = $cookieStore.get('isAuthorization');
         $rootScope.Person = $cookieStore.get('Person');
-        //$rootScope.url = "https://secure-citadel-93919.herokuapp.com/"
-        $rootScope.url = "http://localhost:3001/";
+        $rootScope.url = "https://secure-citadel-93919.herokuapp.com/"
+//        $rootScope.url = "http://localhost:3001/";
 
     })
     .controller("MainCtrl", function ($scope, $rootScope, $cookieStore, $location) {
